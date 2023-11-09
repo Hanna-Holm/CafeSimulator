3ce415f05fbda0d4635430e1da81076643d41a3d
Hanna Holm <hanna.holm@hotmail.com>
2023-11-09 09:55:29 +0100

    Setup of project

---
+++ CafeSimulator/Cafe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeSimulator
+{
+    internal class Cafe
+    {
+        /* TODO:
+         * - Hire method for new employees
+         * - SumOfSalaries that sums the total amount of salary for all employees together
+         * - netWorth should update each time a customer shops, add shop sum to netWorth.
+         * - When buying more products into inventory: decrease total cost from netWorth.
+        */
+        public int NetWorth { get; private set; }
+        public int NumberOfEmployees { get; private set; }
+
+        private List<Fika> _inventory;
+    }
+}
---
+++ CafeSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CafeSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cafe cafe = new Cafe();
+
+            Console.ReadKey();
+        }
+    }
+}
